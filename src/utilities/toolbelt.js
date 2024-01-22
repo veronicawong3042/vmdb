52e813d99a1e986dad9339a183baf95186bc9069
Yui-0807 <131344066+Yui-0807@users.noreply.github.com>
2024-01-22 10:32:18 -0800

    added function description

--- src/utilities/toolbelt.js
+++ src/utilities/toolbelt.js
@@ -1,3 +1,5 @@
+// convert date format
+// e.g. "2004-04-01" to "April 1, 2004"
 function dateConvert (str) {
     const dateString = new Date (Date(str));
     const options = {
@@ -6,7 +8,9 @@ function dateConvert (str) {
     return (dateString).toLocaleDateString("en-US", options);
 ;}
 
-
+// truncate paragraph
+// If the content is over 100 characters, then output the first 100 characters; 
+// otherwise, output all the content
 function truncate (input) {
     return input.length > 100 ? `${input.substring(0, 100)}...` : input;
 }
