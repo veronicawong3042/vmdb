bfbba9afcfdfa4f2b0910942b6085d2901e03953
Yui-0807 <131344066+Yui-0807@users.noreply.github.com>
2024-01-23 17:31:19 -0800

    addjusted

--- src/pages/WorkShop.js
+++ src/pages/WorkShop.js
@@ -1,11 +1,17 @@
+import { useState } from "react";
+import Footer from "../components/Footer";
 import MovieCard from "../components/MovieCard";
 
 function WorkShop() {
     
+// Clear the entire localStorage
+// localStorage.clear();
+
     return (
         <div>
             <h2>WorkShop</h2>
             <MovieCard />
+            <Footer />
         </div>
     );
 }
