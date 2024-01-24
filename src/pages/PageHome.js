105dc5469a09e8e9f78b80538a31243f89815401
Yui-0807 <131344066+Yui-0807@users.noreply.github.com>
2024-01-23 17:31:04 -0800

    adjusted

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { getPopularMovies } from "../utilities/api";
 import MoviesContainer from "../components/MoviesContainer";
+import Footer from "../components/Footer";
 
 function PageHome() {
 
     const [popularMovies, setPopularMovies] = useState([]);
+    
     useEffect(() => {
         getPopularMovies()
             .then((data) => {
@@ -16,9 +18,13 @@ function PageHome() {
             });
     }, []);
 
-    return <main id="home">
-    <MoviesContainer title="Popular Movies" moviesData={popularMovies}/>
+    return (
+    <main id="home">
+        <MoviesContainer title="Popular Movies" moviesData={popularMovies}/>
+        
     </main>
+
+    );
 }
 
 export default PageHome;
