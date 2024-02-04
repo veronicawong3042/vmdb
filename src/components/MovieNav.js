2a9f7190a6667a4e84b2294182e0f7b158d1eb11
veronicawong3042 <150846086+veronicawong3042@users.noreply.github.com>
2024-02-03 16:38:20 -0800

    responsive movie category nav, fixed layout

--- src/components/MovieNav.js
+++ src/components/MovieNav.js
@@ -1,15 +1,53 @@
-const { NavLink } = require("react-router-dom");
+import { useState } from "react";
+import { FaRegStar } from "react-icons/fa";
+import { LiaTrophySolid } from "react-icons/lia";
+import { RxCalendar } from "react-icons/rx";
+import { LuAlarmClock } from "react-icons/lu";
+import { NavLink } from "react-router-dom";
 
-const MovieNav = () => (
-    <nav>
-        <ul>
-            <li><NavLink to="/">All</NavLink></li>
-            <li><NavLink to="/now-playing">Now Playing</NavLink></li>
-            <li><NavLink to="/popular">Popular</NavLink></li>
-            <li><NavLink to="/top-rated">Top Rated</NavLink></li>
-            <li><NavLink to="/upcoming">Upcoming</NavLink></li>
-        </ul>
+const MovieNav = () => {
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
+
+  return (
+    <nav className="movie-categories">
+      <NavLink
+        to="/top-rated"
+        className={`slider-button ${activeButton === "top-rated" ? "active" : ""}`}
+        onClick={() => handleButtonClick("top-rated")}
+      >
+        <LiaTrophySolid />
+        <span>Top Rated</span>
+      </NavLink>
+      <NavLink
+        to="/popular"
+        className={`slider-button ${activeButton === "popular" ? "active" : ""}`}
+        onClick={() => handleButtonClick("popular")}
+      >
+        <FaRegStar />
+        <span>Popular</span>
+      </NavLink>
+      <NavLink
+        to="/now-playing"
+        className={`slider-button ${activeButton === "now-playing" ? "active" : ""}`}
+        onClick={() => handleButtonClick("now-playing")}
+      >
+        <LuAlarmClock />
+        <span>Now Playing</span>
+      </NavLink>
+      <NavLink
+        to="/upcoming"
+        className={`slider-button ${activeButton === "upcoming" ? "active" : ""}`}
+        onClick={() => handleButtonClick("upcoming")}
+      >
+        <RxCalendar />
+        <span>Upcoming</span>
+      </NavLink>
     </nav>
-)
+  );
+};
 
 export default MovieNav;
