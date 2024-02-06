73ae0e4cbfd14e5b70952972704786b536d7ea16
veronicawong3042 <150846086+veronicawong3042@users.noreply.github.com>
2024-02-05 18:36:12 -0800

    fixed movietab buttons and centered favs

--- src/components/CategoryButtons.js
+++ src/components/CategoryButtons.js
@@ -1,21 +1,52 @@
+import { useState } from "react";
+import { FaRegStar } from "react-icons/fa";
+import { LiaTrophySolid } from "react-icons/lia";
+import { RxCalendar } from "react-icons/rx";
+import { LuAlarmClock } from "react-icons/lu";
+
 function CategoryButtons ({categoryName = 'Popular', setCategoryName}){
+
+    const [activeButton, setActiveButton] = useState("popular");
+    
+    const handleButtonClick = (buttonName) => {
+        setActiveButton(buttonName);
+      };
+
     return(
-        <div className="category-buttons">
-            <button className='popular-btn' onClick={()=>{
-                setCategoryName('Popular')
-            }}>Popular</button>
+        <div className="movie-categories">
+            <button className={`slider-button ${activeButton === "popular" ? "active" : ""}`} 
+            onClick={()=>{
+                handleButtonClick("popular")
+                
+            }}>
+                <FaRegStar />
+                <p>Popular</p>
+                </button>
             
-            <button className='now-playing-btn' onClick={()=>{
-                setCategoryName('Now Playing')
-            }}>Now Playing</button>
+            <button className={`slider-button ${activeButton === "now-playing" ? "active" : ""}`} onClick={()=>{
+                handleButtonClick("now-playing")
+                
+            }}>
+                <LuAlarmClock />
+                <p>Now Playing</p>
+                </button>
 
-            <button className='upcoming-btn' onClick={()=>{
-                setCategoryName('Upcoming')
-            }}>Upcoming</button>
+            <button className={`slider-button ${activeButton === "upcoming" ? "active" : ""}`} onClick={()=>{
+                handleButtonClick("upcoming")
+                
+            }}>
+                <RxCalendar />
+                <p>Upcoming</p>
+                </button>
 
-            <button className="top-rated-btn" onClick={()=>{
-                setCategoryName('Top Rated')
-            }}>Top Rated</button>
+            <button className={`slider-button ${activeButton === "top-rated" ? "active" : ""}`}
+             onClick={()=>{
+                handleButtonClick("top-rated")
+                
+            }}>
+                <LiaTrophySolid />
+                <p>Top Rated</p>
+                </button>
         </div>
     )
 }
