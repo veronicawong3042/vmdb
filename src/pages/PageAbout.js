057169fe1bf3c7c7ea3bcf2a82820fd47b1750e4
Yui-0807 <131344066+Yui-0807@users.noreply.github.com>
2024-02-08 12:30:50 -0800

    added about page backgroung icon

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -1,9 +1,14 @@
+import { PiFilmSlate } from "react-icons/pi";
+
 function PageAbout(){
    
 
     return(
         <div className="about">
             <h1>About</h1>
+            <div className="about-bg-icon">
+            <PiFilmSlate />
+            </div>
             <p>Veronica’s Movie Database (VMDB) is your go-to hub for all things cinema. Explore a diverse range of films from classics to contemporary hits. With user-friendly navigation, discover detailed information, reviews, and ratings for each movie. Stay updated with the latest news, trailers, and release dates. Engage with a vibrant community of movie enthusiasts and share your thoughts and recommendations. VMDB is your ultimate companion for a seamless movie-watching experience. Dive into the world of cinema today and let VMDB be your guide to endless entertainment.</p>
         </div>
     )
